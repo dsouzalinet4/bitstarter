15ee4fa644b34144cfc36988315680912d8c66c2
Linet Dsouza <dsouzalinet4@gmail.com>
2013-07-21 04:43:14 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,15 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-	var fs = require('fs');)
+	var fs = require('fs');
 	fs.readFile('index.html', function (err, data) {
-  		if (err) throw err;
   		var buff = new Buffer(data);
 		response.send(buff.toString());
 		
 	});
-  	if (err) throw err;
-  		console.log('It\'s saved!');
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
